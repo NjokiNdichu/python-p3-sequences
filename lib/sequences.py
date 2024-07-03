b0c1b116e06ba5de378db3e76c10c9e4c8cb6cda
Njoki Ndichu <rejoicenjoki0@gmail.com>
2024-07-03 20:28:59 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
+    else:
+        sequence = [0, 1]
+        for i in range(2, length):
+            sequence.append(sequence[i-1] + sequence[i-2])
+        print(f"[{', '.join(map(str, sequence))}]")
+
+
+print_fibonacci(2)
